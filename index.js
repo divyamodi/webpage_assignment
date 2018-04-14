418d6f32adf66396c830563970452061dd907df1
DIVYA MODI <divya.belkin.modi@gmail.com>
2018-04-14 21:15:46 +0530

    Comment box added

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-$('.video').parent().click(function () {
-  if($(this).children(".video").get(0).paused){        $(this).children(".video").get(0).play();   $(this).children(".playpause").fadeOut();
-    }else{       $(this).children(".video").get(0).pause();
-  $(this).children(".playpause").fadeIn();
-    }
-});
+// $('.video').parent().click(function () {
+//   if($(this).children(".video").get(0).paused){        $(this).children(".video").get(0).play();   $(this).children(".playpause").fadeOut();
+//     }else{       $(this).children(".video").get(0).pause();
+//   $(this).children(".playpause").fadeIn();
+//     }
+// });
 
-$(document).ready(function() {
-	$('textarea').shiftenter();
-	$("#comment-form").on('submit', function(event){
-		event.preventDefault();
-    	alert($("#comment-box").text());
-	});
+// $(document).ready(function() {
+// 	$('textarea').shiftenter();
+// 	$("#comment-form").on('submit', function(event){
+// 		event.preventDefault();
+//     	alert($("#comment-box").text());
+// 	});
+// });
+
+
+$(document).ready(function(){
+	var nameArray = ["divya", "onil", "deepa"];
+	var localStr = "webpage_assignment";
+	if(localStorage.getItem(localStr) === null)
+	{
+		var name = nameArray[Math.floor(Math.random()*nameArray.length)];
+		obj = {"name": name};
+		localStorage.setItem(localStr, JSON.stringify(obj));
+	}
+	else
+	{
+		console.log(JSON.parse(localStorage.getItem(localStr)));
+	}
 });
